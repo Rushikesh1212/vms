db93bdf8c6dfdbf175caad204fa6751e5beb55a5
Dnyaneshwar Kadam <kadamdnyanesh248@gmail.com>
2019-10-12 10:44:21 +0530

    removed user management

--- vmsadmin/src/admin/contactList/ContactList.js
+++ vmsadmin/src/admin/contactList/ContactList.js
@@ -11,45 +11,13 @@ class ContactList extends Component {
 	constructor(props){
 		super(props);
 		this.state = {
-        "mobileNumber"    : "",
-        "whatsAppNumber"  : "",
-        "dead"            : false,
-        "visited"         : true,
-        "voted"           : false,
-        "changeAddress"   : "",
-        "areaName"        : "",
-        "otherInfo"       : "",
-        "dob"             : "21-10-2001",
-        "emailId"         : "",
-        "aadharCard"      : "",
-        "color"           : 2,
-        "cast"            : "",
-        "featured"        : false,
-        voter_id          : "",
-        villageList       : "",
-        allPosts          : [],
-        info              : [],
-        editUser          : "",
-        show              : true,
-        firstname         : "",
-        lastname          : "",
-        signupEmail       : "",
-        mobNumber         : "",
-        action            : "Submit",
-        toggleEventHandle : "",
-        toggleStatus      : "off",
-        formerrors        :{
-                            firstName:"",
-                            lastName:"",
-                            emailId:"",
-                            mobileNumber:"",
-                            role: "User",
-                           },
-      voterName           : "",
-      voterId             : "",                        
+        loading             : false,
+        villageName         : "",
+        allVillageList      : [],
+        allVotersList       : [],
+        villageWiseViterList: [],                        
 		}
     this.handleChange = this.handleChange.bind(this);
-    this.handleSearch = this.handleSearch.bind(this);
 	}
 
   componentDidMount(){	
@@ -57,11 +25,8 @@ class ContactList extends Component {
       .get('/api/voters/villagelist')
       .then(
         (res)=>{
-          console.log('res ==', res.data);
-          const postsdata = res.data;
-          // console.log('postsdata',postsdata);
           this.setState({
-            allPosts : postsdata,
+            allVillageList : res.data,
           });         
         }
       )
@@ -75,118 +40,29 @@ class ContactList extends Component {
     event.preventDefault();
     const datatype = event.target.getAttribute('data-text');
     const {name,value} = event.target;
-    let formerrors = this.state.formerrors;
-    
     console.log("value",value);
-    this.setState({ formerrors,
-      [name]:value
-    });
-    axios
-      .get('/api/voters/distinctBooth')
-      .then((res)=>{
-          console.log("response123 = ",res.data);
-            $('body').removeClass("modal-open");
-            this.setState({
-              "mobileNumber"    : "",
-              "whatsAppNumber"  : "",
-              "featured"        : false,
-            })
-          
-      })
-      .catch((error)=>{
-        console.log("error = ",error);
-        this.setState({show: false})
-      });
-  }
-
-  handleSearch(event){
-    event.preventDefault();
-   const {name,value} = event.target;
-   this.setState({
-      [name]:value
-    },()=>{
-      var formValues = {
-        voterName :this.state.voterName,
-        idNumber  :this.state.voterId,
-        "featured"    :"",
-        "mobileNumber":"",
-        "voted"       :"",
-        "visited"     :"",
-        "dead"        :"",
-        "aadharCard"  :"",
-        "cast"        :"",
-        "areaName"    :"",
-        "boothName"   :"",
-        "voterAgeFrom":"",
-      }
-       axios
-      .post('/api/search/voters',formValues)
-      .then(
-        (res)=>{
-          console.log('res', res.data);
-          const postsdata = res.data;
-          // console.log('postsdata',postsdata);
-          this.setState({
-            allPosts : postsdata,
-          });         
-        }
-      )
-      .catch((error)=>{
-        console.log("error = ",error);
-        // alert("Something went wrong! Please check Get URL.");
-      });
-    });
- 
-  }
-
-	voterList(voterList){
-      $('body').removeClass("modal-open");
-
-		  this.setState({
-        allPosts:voterList
-      })
-    }
-
-	closeModal(event){
-		 $('#deleteModal').removeClass("in");
-		 $('#deleteModal').css("display","none");
-	}
-
-  updateData(event){
-    event.preventDefault();
-    const formValues = {
-        "voter_id"        : this.state.voter_id,
-        "mobileNumber"    : this.state.mobileNumber,
-        "whatsAppNumber"  : this.state.whatsAppNumber,
-       }
-       console.log("dk == ",formValues);
-        
-  }
-
-  handleEdit(row){
-    console.log("row",row);
-    // var userData=row.profile;
+    this.setState({"loading" : true})
     this.setState({
-      "mobileNumber"    : row.mobileNumber,
-      "whatsAppNumber"  : row.whatsAppNumber,
-      "dead"            : row.dead,
-      "visited"         : true,
-      "voted"           : row.voted,
-      "changeAddress"   : row.changeAddress,
-      "areaName"        : row.areaName,
-      "otherInfo"       : row.otherInfo,
-      "dob"             : moment(row.dob).format('YYYY-MM-DD'),
-      "emailId"         : row.emailId,
-      "aadharCard"      : row.aadharCard,
-      "color"           : row.color,
-      "cast"            : row.cast,
-      "featured"        : row.featured,
-      info              : row,
-      voter_id          : row._id,
-    })
+      [name]:value
+    });
+    var villageName ={villageName:value};
+    axios
+      .post('/api/voters/voterslist',villageName)
+      .then((res)=>{
+        console.log("response123 = ",res.data);
+        this.setState({
+          "villageWiseViterList" : res.data,
+          "loading"              : false
+        })
+      })
+      .catch((error)=>{
+        console.log("error = ",error);
+        this.setState({"loading" : false})
+      });
   }
+
 	render(){
-	  const data = this.state.allPosts;
+	  const data = this.state.villageWiseViterList;
 	  const columns = [{
         Header: 'Voting Card No.',
         accessor: 'idNumber',
@@ -199,6 +75,9 @@ class ContactList extends Component {
       },{
         Header: 'Mobile No.',
         accessor: 'mobileNumber',
+      },{
+        Header: 'WhatsApp No.',
+        accessor: 'whatsAppNumber',
       }]
 
 	var adminRolesListDataList = this.state.adminRolesListData;
@@ -218,11 +97,10 @@ class ContactList extends Component {
                     <i className="fa fa-envelope-square"></i>
                   </div>
                   <select type="text" className="formFloatingLabels form-control newinputbox" 
-                  ref="villageList" name="villageList" id="villageList" data-text="villageList" onChange={this.handleChange}  value={this.state.villageList}
-                  placeholder="Enter Caste">
-                    <option>----Select----</option>
-                  {this.state.allPosts.map((villagelist)=>
-                    <option>{villagelist}</option>
+                  ref="villageName" name="villageName" id="villageName" data-text="villageName" onChange={this.handleChange}  value={this.state.villageName}>
+                    <option selected>----Select Village----</option>
+                  {this.state.allVillageList.map((villageName)=>
+                    <option>{villageName}</option>
                     )
                   }
                   </select>
@@ -233,11 +111,8 @@ class ContactList extends Component {
         <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12 paddingright">
           <h4 className="usrmgnttitle weighttitle">List of Voters: </h4>
         </div>
+        {this.state.loading===false?
         <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-        {/*<div class="progress md-progress primary-color-dark">
-            <div class="indeterminate"></div>
-        </div>
-        <canvas id="barChart"></canvas>*/}
   				<ReactTable
 				    data={data}
 				    columns={columns}
@@ -250,6 +125,11 @@ class ContactList extends Component {
             className ={"-striped -highlight"}
 				  />
   		  </div>
+        :
+        <div className="col-lg-6 col-lg-offset-3">
+          <img src="/images/loader.gif" width="100%" height="100%"/>
+        </div>
+      }
       </div>
 	  );
 	}
